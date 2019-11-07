c8b612b7a7790d4ec201893cd5fc2da4d303cbf5
vanellalisia <43409193+vanellalisia@users.noreply.github.com>
2019-11-08 01:36:09 +0700

    pisah headbar pelanggan

---
+++ application/views/headerbar_pelanggan.php
@@ -0,0 +1,34 @@
+<div class="site-wrap">
+      
+      <nav class="site-menu">
+        <div class="site-menu-inner">
+          <ul class="list-unstyled">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="menu.html">Our Menu</a></li>
+            <li><a href="blog.html">Our Blog</a></li>
+            <li><a href="reservation.html">Reserve A Table</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <header class="site-header">
+        <div class="row align-items-center">
+          <div class="col-5 col-md-3">
+             <ul class="list-unstyled social">
+              <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+              <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+              <li><a href="#"><span class="fa fa-instagram"></span></a></li>
+            </ul>
+          </div>
+          <div class="col-2 col-md-6 text-center">
+            <a href="index.html" class="site-logo"><img src="<?php echo base_url('assets/images/logoserviam2.png')?>"></a>
+          </div>
+          <div class="col-5 col-md-3 text-right menu-burger-wrap">
+            <a href="#" class="site-nav-toggle js-site-nav-toggle"><i></i></a>
+
+          </div>
+        </div>
+       
+      </header>
