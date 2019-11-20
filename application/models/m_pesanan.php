774eeb121b93ead1d8a9f956f289dce13f281e55
vanellalisia <43409193+vanellalisia@users.noreply.github.com>
2019-11-20 12:45:00 +0700

    Update m_pesanan.php

--- application/models/m_pesanan.php
+++ application/models/m_pesanan.php
@@ -4,10 +4,10 @@ class m_pesanan extends CI_Model{
         return $this->db->query("SELECT * FROM pesanan WHERE tanggal_pesan LIKE '$tanggalpesan%'");
     }
     function tambah_pesanan($table, $data_pesanan){
-        return $this->db->insest($table, $data_pesanan);
+        return $this->db->insert($table, $data_pesanan);
     }
     function tambah_detail_pesanan($pesanan, $table, $data){
-        
+        return $this->db->insert($table, $pesanan, $data);
     }
 }
 ?>
